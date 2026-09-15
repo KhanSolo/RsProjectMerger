0e4965842f0d52b6aa87e6bebeeed55a7f436753
Konstantin <drpdrp@mail.ru>
2026-09-15 22:59:29 +0300

    ADD: some input logic

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 use std::env;
+use std::path::Path;
 use std::process::ExitCode;
+
+static IGNORED_DIRECTORIES : &'static [&str] = &[
+        "bin",
+        "obj",
+        ".git",
+        ".vs"
+];
 
 fn main() -> ExitCode {
     let args : Vec<String> = env::args().collect();
-    // for arg in &args {
-    //     println!("{}", &arg);
-    // }
 
-    if args.len() < 1 || args.len() > 2 {
+    // args[0] - executable
+    if args.len() < 2 || args.len() > 3 {
         print_usage();
         return ExitCode::FAILURE;
     }
+
+    let input_file_path = Path::new(&args[1]);
+    if !input_file_path.exists() {
+        println!("Input file not found {}", input_file_path.display());
+        return ExitCode::FAILURE;
+    }
+
+    if let Some(extension ) =  input_file_path.extension() {
+        if extension == ".csproj" || extension == ".sln" { // todo: case insensitive
+        } else {
+            println!("Extension {} is not supported", extension.display());
+            return ExitCode::FAILURE;
+        }
+    } else {
+        println!("Cannot get extension of the {}", input_file_path.display());
+        return ExitCode::FAILURE;
+    }
+
+    let output_path = if args.len() == 3 {
+        "" // todo: from 3rd arg
+    } else {
+        "merged-project.txt" // todo: add parent folder from input path
+    };
 
     
 
