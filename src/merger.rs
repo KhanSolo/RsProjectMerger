8bbe1a0799ed58fb60579bcb33c49f25ebc7ed9e
Konstantin <drpdrp@mail.ru>
2026-09-20 02:15:32 +0300

    TECH: append_project method

--- src/merger.rs
+++ src/merger.rs
@@ -1,5 +1,5 @@
 use std::fmt::Write;
-use std::fs::File;
+use std::fs::{File,read_to_string};
 use std::io::{BufRead, BufReader, Result};
 use std::path::{Path, PathBuf};
 
@@ -33,7 +33,7 @@ pub fn find_projects_in_solution(solution_path: &Path) -> Result<Vec<PathBuf>> {
 }
 
 pub fn build_merged_file(projects: Vec<&Path>, output_path: &Path) -> String {
-    let mut builder = String::new();
+    let mut builder = String::with_capacity(projects.len()*1024);
     for project_path in projects {
         append_project(&mut builder, project_path, output_path);
     }
@@ -41,7 +41,45 @@ pub fn build_merged_file(projects: Vec<&Path>, output_path: &Path) -> String {
 }
 
 pub fn append_project(builder:&mut String, project_path: &Path, output_full_path: &Path) {
+    let project_directory = project_path.parent().unwrap_or(Path::new("."));
 
+    // .csproj
+    let Ok(relative_path) = project_path.strip_prefix(project_directory) else {
+        eprintln!("Cannot get relative_path");
+        return; // todo: create error
+    };
+    append_header(builder, relative_path);
+
+    let Ok(file_all_text) = read_to_string(project_path) else {
+        eprintln!("Cannot get file_all_text");
+        return; // todo: create error
+    };
+    let _ = writeln!(builder, "{}", file_all_text.trim());
+    let _ = writeln!(builder, ""); // new line
+
+    // .cs files
+    let source_files: Vec<&Path> = vec![];
+    /*   var sourceFiles = Directory
+    .EnumerateFiles(projectDirectory, "*.cs", SearchOption.AllDirectories)
+    .Where(file => !IsIgnoredDirectory(file))
+    .Where(file => !Path.GetFullPath(file).Equals(outputPath, StringComparison.OrdinalIgnoreCase))
+    .OrderBy(file => file, StringComparer.OrdinalIgnoreCase)
+    .ToList(); */        
+    for source_file in source_files
+    {
+        let Ok(relative_path) = source_file.strip_prefix(project_directory) else {
+            eprintln!("Cannot get relative_path");
+            return; // todo: create error
+        };
+        append_header(builder, relative_path);
+
+        let Ok(file_all_text) = read_to_string(project_path) else {
+            eprintln!("Cannot get file_all_text");
+            return; // todo: create error
+        };
+        let _ = writeln!(builder, "{}", file_all_text.trim());
+        let _ = writeln!(builder, ""); // new line      
+    }
 }
 
 pub fn append_header(builder:&mut String, file_path: &Path) {
